09d1db1c8e22fe9df87574f75eb9dcce0736f3a3
Karol594 <karagodlewski@gmail.com>
2025-05-23 00:06:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import os
 from telegram.ext import ApplicationBuilder
+from handlers.start import register_start_handlers
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 application = ApplicationBuilder().token(BOT_TOKEN).build()
 
+# Барлық хендлерлерди тіркеў
+register_start_handlers(application)
+
+# Ботты іске қосыў
 application.run_polling()
